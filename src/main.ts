99a3eb58296808a9b23437d0e1a97b6131cffea0
Félix <felixgraphx@gmail.com>
2024-11-16 12:32:24 -0500

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,46 @@
+const vertexShaderSource = /*glsl*/ `#version 300 es
+
+void main()
+{
+    gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
+    gl_PointSize = 150.0;
+}`;
+
+const fragmentShaderSource = /*glsl*/ `#version 300 es
+
+precision mediump float;
+
+out vec4 fragColor;
+
+void main()
+{
+    fragColor = vec4(1.0, 0.0, 0.0, 1.0);
+}`;
+
+// start
+const canvas = document.querySelector('canvas')!;
+const gl = canvas.getContext('webgl2')!;
+
+const program = gl.createProgram()!;
+
+const vertexShader = gl.createShader(gl.VERTEX_SHADER)!;
+gl.shaderSource(vertexShader, vertexShaderSource);
+gl.compileShader(vertexShader);
+gl.attachShader(program, vertexShader);
+
+const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER)!;
+gl.shaderSource(fragmentShader, fragmentShaderSource);
+gl.compileShader(fragmentShader);
+gl.attachShader(program, fragmentShader);
+
+gl.linkProgram(program);
+
+if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+    console.log(gl.getShaderInfoLog(vertexShader));
+    console.log(gl.getShaderInfoLog(fragmentShader));
+    console.log(gl.getProgramInfoLog(program));
+}
+
+gl.useProgram(program);
+
+gl.drawArrays(gl.POINTS, 0, 1);
