363c86a360e04da942414f3376d885de3fd78f10
Félix <felixgraphx@gmail.com>
2024-11-19 19:33:38 -0500

    Created basic hello world to reset main.ts as needed.

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,10 @@
 // VERTEX SHADER
 const vertexShaderSource = /*glsl*/ `#version 300 es
 
-layout(location = 0) in vec4 aPosition;
-layout(location = 1) in vec4 aColor;
-
-out vec4 vColor;
-
 void main()
 {
-    vColor = aColor;
-    gl_Position = aPosition;
+    gl_PointSize = 150.0;
+    gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
 }`;
 
 // FRAGMENT SHADER
@@ -17,13 +12,11 @@ const fragmentShaderSource = /*glsl*/ `#version 300 es
 
 precision mediump float;
 
-in vec4 vColor;
-
 out vec4 fragColor;
 
 void main()
 {
-    fragColor = vColor;
+    fragColor = vec4(1.0, 0.0, 0.0, 1.0);
 }`;
 
 // Start
@@ -52,66 +45,5 @@ if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
 
 gl.useProgram(program);
 
-// For usage with drawArrays
-const arrayVertexData = new Float32Array([
-    0, 0, 1, 0, 0,
-    0.0, 1.0, 1, 0, 0,
-    0.951, 0.309, 1, 0, 0,
+gl.drawArrays(gl.POINTS, 0, 1);
 
-    0, 0, 0, 1, 0,
-    0.951, 0.309, 0, 1, 0,
-    0.587, -0.809, 0, 1, 0,
-
-    0, 0, 0, 0, 1,
-    0.587, -0.809, 0, 0, 1,
-    -0.587, -0.809, 0, 0, 1,
-
-    0, 0, 1, 1, 0,
-    -0.587, -0.809, 1, 1, 0,
-    -0.951, 0.309, 1, 1, 0,
-
-    0, 0, 1, 0, 1,
-    -0.951, 0.309, 1, 0, 1,
-    0.0, 1.0, 1, 0, 1,
-
-]);
-
-const arrayVertexDataBuffer = gl.createBuffer()!;
-gl.bindBuffer(gl.ARRAY_BUFFER, arrayVertexDataBuffer);
-gl.bufferData(gl.ARRAY_BUFFER, arrayVertexData, gl.STATIC_DRAW);
-
-// For usage with drawElements
-const elementVertexData = new Float32Array([
-    0, 0, 0, 0, 0,
-    0.0, 1.0, 1, 0, 0,
-    0.951, 0.309, 0, 1, 0,
-    0.587, -0.809, 0, 0, 1,
-    -0.587, -0.809, 1, 1, 0,
-    -0.951, 0.309, 1, 0, 1,
-]);
-
-const elementIndexData = new Uint8Array([
-    0, 1, 2,
-    0, 2, 3,
-    0, 3, 4,
-    0, 4, 5,
-    0, 5, 1,
-]);
-
-const elementVertexBuffer = gl.createBuffer()!;
-gl.bindBuffer(gl.ARRAY_BUFFER, elementVertexBuffer);
-gl.bufferData(gl.ARRAY_BUFFER, elementVertexData, gl.STATIC_DRAW);
-
-const elementIndexBuffer = gl.createBuffer()!;
-gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, elementIndexBuffer);
-gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, elementIndexData, gl.STATIC_DRAW);
-
-// gl.bindBuffer(gl.ARRAY_BUFFER, arrayVertexBufferData);
-gl.vertexAttribPointer(0, 2, gl.FLOAT, false, 5 * 4, 0);
-gl.vertexAttribPointer(1, 3, gl.FLOAT, false, 5 * 4, 2 * 4);
-
-gl.enableVertexAttribArray(0);
-gl.enableVertexAttribArray(1);
-
-// gl.drawArrays(gl.TRIANGLES, 0, 15);
-gl.drawElements(gl.TRIANGLES, 15, gl.UNSIGNED_BYTE, 0);
