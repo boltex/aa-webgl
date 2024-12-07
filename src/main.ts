c972be676827438f0ce93629a891f78406137e53
Félix <felixgraphx@gmail.com>
2024-12-07 01:02:43 -0500

    Chapter12 : Added two different methods !

--- src/main.ts
+++ src/main.ts
@@ -41,6 +41,15 @@ function loadImage(src: string): Promise<HTMLImageElement> {
     });
 }
 
+function getImageData(image: HTMLImageElement): Uint8ClampedArray {
+    const canvas = document.createElement('canvas');
+    canvas.width = image.width;
+    canvas.height = image.height;
+    const context = canvas.getContext('2d')!;
+    context.drawImage(image, 0, 0);
+    return context.getImageData(0, 0, image.width, image.height).data;
+}
+
 (async () => {
 
     const canvas = document.querySelector('canvas')!;
@@ -80,11 +89,28 @@ function loadImage(src: string): Promise<HTMLImageElement> {
     // * Start Program *
 
     // const image = await loadImage('images/alien-vertical.png');
-    const image = await loadImage('images/plancher-vertical.png'); // TOO BIG !
+    // const image = await loadImage('images/plancher-vertical.png');
+    const image = await loadImage('images/plancher2.png'); // plancher 2 is 24 bits
+    const imageData = getImageData(image);
 
     const texture = gl.createTexture()!;
     gl.bindTexture(gl.TEXTURE_2D_ARRAY, texture);
-    gl.texImage3D(gl.TEXTURE_2D_ARRAY, 0, gl.RGBA, 128, 128, 64, 0, gl.RGBA, gl.UNSIGNED_BYTE, image);
+    gl.texStorage3D(gl.TEXTURE_2D_ARRAY, 1, gl.RGBA8, 128, 128, 64);
+
+    const pbo = gl.createBuffer()!;
+    gl.bindBuffer(gl.PIXEL_UNPACK_BUFFER, pbo);
+    gl.bufferData(gl.PIXEL_UNPACK_BUFFER, imageData, gl.STATIC_DRAW);
+    gl.pixelStorei(gl.UNPACK_ROW_LENGTH, image.width);
+    gl.pixelStorei(gl.UNPACK_IMAGE_HEIGHT, image.height);
+
+    for (let y = 0; y < 8; y++) {
+        for (let x = 0; x < 8; x++) {
+            gl.pixelStorei(gl.UNPACK_SKIP_PIXELS, x * 128);
+            gl.pixelStorei(gl.UNPACK_SKIP_ROWS, y * 128);
+            gl.texSubImage3D(gl.TEXTURE_2D_ARRAY, 0, 0, 0, y * 8 + x, 128, 128, 1, gl.RGBA, gl.UNSIGNED_BYTE, 0);
+        }
+    }
+
     gl.texParameteri(gl.TEXTURE_2D_ARRAY, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
     gl.texParameteri(gl.TEXTURE_2D_ARRAY, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
 
@@ -95,6 +121,7 @@ function loadImage(src: string): Promise<HTMLImageElement> {
     gl.vertexAttribPointer(1, 2, gl.FLOAT, false, 16, 8);
     gl.enableVertexAttribArray(0);
     gl.enableVertexAttribArray(1);
+    gl.vertexAttrib1f(2, 0); // Depth: change second parameter to change sprite.
 
     // Transparency
     gl.enable(gl.BLEND);
