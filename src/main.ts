72d2feadb60f1d738332fc168cb4cd5b4b00611f
Félix <felixgraphx@gmail.com>
2024-12-12 22:20:08 -0500

    Made an instanced draw setup with large 4096 square texture!

--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,16 @@
 const vertexShaderSource = /*glsl*/ `#version 300 es
 
 layout(location=0) in vec4 aPosition;
-layout(location=1) in vec3 aOffset;
-layout(location=2) in float aScale;
-layout(location=3) in vec4 aColor;
-layout(location=4) in vec2 aTexCoord;
-layout(location=5) in float aDepth;
+layout(location=1) in vec2 aTexCoord;
+layout(location=2) in vec3 aOffset;
+layout(location=3) in float aScale;
+layout(location=4) in vec2 aUV;
 
-out vec4 vColor;
 out vec2 vTexCoord;
-out float vDepth;
 
 void main()
 {
-    vColor = aColor;
-    vTexCoord = aTexCoord;
-    vDepth = aDepth;
+    vTexCoord = vec2(aTexCoord * 0.015625) + aUV;
     gl_Position = vec4(aPosition.xyz * aScale + aOffset, 1.0);
 }`;
 
@@ -25,16 +20,15 @@ const fragmentShaderSource = /*glsl*/ `#version 300 es
 
 precision mediump float;
 
-uniform mediump sampler2DArray uSampler;
+uniform mediump sampler2D uSampler;
 
-in vec4 vColor;
 in vec2 vTexCoord;
-in float vDepth;
+
 out vec4 fragColor;
 
 void main()
 {
-    fragColor = texture(uSampler, vec3(vTexCoord, vDepth));
+    fragColor = texture(uSampler, vTexCoord);
 }`;
 
 // Load image asynchronously
@@ -45,7 +39,6 @@ function loadImage(src: string): Promise<HTMLImageElement> {
         image.src = src;
     });
 }
-
 (async () => {
 
     const canvas = document.querySelector('canvas')!;
@@ -75,55 +68,59 @@ function loadImage(src: string): Promise<HTMLImageElement> {
 
     // * Start Program *
 
-    const image = await loadImage('images/plancher-vertical.png');
+    const image = await loadImage('images/alien.png');
 
     const texture = gl.createTexture()!;
-    gl.bindTexture(gl.TEXTURE_2D_ARRAY, texture);
-    gl.texImage3D(gl.TEXTURE_2D_ARRAY, 0, gl.RGBA, 128, 128, 64, 0, gl.RGBA, gl.UNSIGNED_BYTE, image); // 64 textures of 128x128 pixels
-    gl.texParameteri(gl.TEXTURE_2D_ARRAY, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-    // gl.texParameteri(gl.TEXTURE_2D_ARRAY, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 4096, 4096, 0, gl.RGBA, gl.UNSIGNED_BYTE, image);
+    gl.generateMipmap(gl.TEXTURE_2D);
 
     const modelData = new Float32Array([
-        // Position   texCoord
-        -1, -0.7, 0, 1,
-        0, 0.8, 0.5, 0,
-        1, -0.7, 1, 1,
+        // XY Coords, UV Offset to be multiplied by 0.015625 and added to the UV coordinates
+        -1, 0, 0, 1,
+        0, 1, 1, 0,
+        -1, 1, 0, 0,
+        // XY   UV
+        -1, 0, 0, 1,
+        0, 0, 1, 1,
+        0, 1, 1, 0,
     ]);
 
+    const u = (sprite: number, orientation: number) => { return ((sprite % 16) * 0.015625) + (orientation % 4) * 0.25; }
+
+    const v = (sprite: number, orientation: number) => { return (Math.floor(sprite / 16) * 0.015625) + Math.floor(orientation / 4) * 0.25; }
+
     const transformData = new Float32Array([
-        // posX, posY, scale, colorR, colorG, colorB, depth
-        -0.2, 0.7, 0.1, 1, 0, 0, 0,
-        0.3, -0.5, 0.4, 0, 0, 1, 1,
-        -0.4, 0.2, 0.2, 1, 0, 1, 22,
+        // posX, posY, scale, U(frame, orientation), V(frame, orientation) Usually set by game engine.
+        0.2, -0.5, 0.1, u(0, 0), v(0, 0),
+        0.2, 0.5, 0.1, u(0, 1), v(0, 1),
+        0.4, 0.2, 0.1, u(1, 1), v(1, 1),
     ]);
 
     const modelBuffer = gl.createBuffer(); // Create a buffer
     gl.bindBuffer(gl.ARRAY_BUFFER, modelBuffer); // Bind the buffer (meaning "use this buffer" for the following operations)
     gl.bufferData(gl.ARRAY_BUFFER, modelData, gl.STATIC_DRAW); // Put data in the buffer
-    gl.vertexAttribPointer(0, 2, gl.FLOAT, false, 16, 0); // Describe the data in the buffer with a size of 2 because it's a 2D model, and a stride and offset of 0 because the data is tightly packed
-    gl.vertexAttribPointer(4, 2, gl.FLOAT, false, 16, 8);
+    gl.vertexAttribPointer(0, 2, gl.FLOAT, false, 16, 0);  // XY
+    gl.vertexAttribPointer(1, 2, gl.FLOAT, false, 16, 8); // UV Offset
     gl.enableVertexAttribArray(0); // Enable the attribute which is bound to the buffer
-    gl.enableVertexAttribArray(4);
+    gl.enableVertexAttribArray(1);
 
     const transformBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, transformBuffer); // Bind the buffer (meaning "use this buffer for the following operations")
     gl.bufferData(gl.ARRAY_BUFFER, transformData, gl.STATIC_DRAW);
-    gl.vertexAttribPointer(1, 2, gl.FLOAT, false, 28, 0); // Describe the data in the buffer: the x and y coordinates after no offset.
-    gl.vertexAttribPointer(2, 1, gl.FLOAT, false, 28, 8); // Describe the data in the buffer, the scale (after the 8 bytes of the 2 floats for x and y)
-    gl.vertexAttribPointer(3, 3, gl.FLOAT, false, 28, 12); // Describe the data in the buffer, the color (after 12 bytes of the 2 floats for x and y and the 1 float for scale)
-    gl.vertexAttribPointer(5, 1, gl.FLOAT, false, 28, 24); // Describe the data in the buffer, the color (after 12 bytes of the 2 floats for x and y and the 1 float for scale)
+    gl.vertexAttribPointer(2, 2, gl.FLOAT, false, 20, 0); // Describe the data in the buffer: the x and y coordinates after no offset.
+    gl.vertexAttribPointer(3, 1, gl.FLOAT, false, 20, 8); // Describe the data in the buffer, the scale (after the 8 bytes of the 2 floats for x and y)
+    gl.vertexAttribPointer(4, 3, gl.FLOAT, false, 20, 12); // Describe the data in the buffer
 
-    gl.vertexAttribDivisor(1, 1); // Tell the GPU to update the position attribute every instance
-    gl.vertexAttribDivisor(2, 1); // Tell the GPU to update the scale attribute every instance
-    gl.vertexAttribDivisor(3, 1); // Tell the GPU to update the color attribute every instance
-    gl.vertexAttribDivisor(5, 1); // Tell the GPU to update the color attribute every instance
+    gl.vertexAttribDivisor(2, 1); // Tell the GPU to update the position attribute every instance
+    gl.vertexAttribDivisor(3, 1); // Tell the GPU to update the scale attribute every instance
+    gl.vertexAttribDivisor(4, 1); // Tell the GPU to update the color attribute every instance
 
-    gl.enableVertexAttribArray(1); // Enable the attribute
     gl.enableVertexAttribArray(2); // Enable the attribute
     gl.enableVertexAttribArray(3); // Enable the attribute
-    gl.enableVertexAttribArray(5); // Enable the attribute
+    gl.enableVertexAttribArray(4); // Enable the attribute
 
-    gl.drawArraysInstanced(gl.TRIANGLES, 0, 3, 3);
+    gl.drawArraysInstanced(gl.TRIANGLES, 0, 6, 3);
 
     // * End Program *
 
